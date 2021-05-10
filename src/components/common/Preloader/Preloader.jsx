0f8d14976f9325f2a488038436870128c3e829fa
zznmga <zznmga@gmail.com>
2021-05-10 22:49:49 +0300

    Preloader on CSS (57)

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import loading from '../../../assets/images/loading.gif';
+import style from './Preloader.module.css';
 
 let Preloader = (props) => {
   return (
-    <div style={{ backgroundColor: 'white' }}>
-      <img src={loading} />
-    </div>
+    // <div style={{ backgroundColor: 'white' }}>
+    //   <img src={loading} />
+    // </div>
+    <div className={style.ldsdualring}></div>
   );
 };
 
